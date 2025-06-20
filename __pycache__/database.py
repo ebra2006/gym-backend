9483e0c1b2f644cc2408d13da3a60e985582a812
ebra2006 <ebrahimzaid02@gmail.com>
2025-06-20 15:24:00 +0300

    🆕 إضافة ملفات crud.py و main.py وتحديث السيرفر

--- __pycache__/database.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import SQLModel, create_engine
-
-DATABASE_URL = "sqlite:///database.db"
-engine = create_engine(DATABASE_URL, echo=False)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
